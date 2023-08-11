6f7cb942789d9741ffc6ab11c239707a573cc16c
Ishan Choudhary <ishan.dchoudhary@gmail.com>
2023-08-11 17:10:54 +0530

    Created foooter and Navbar. TODO: Fix input in Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from './components/Navbar';
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  
+  //TODO: FIX WEIRD PAGE LENGTH ISSUE
+  //TODO: CREATE NAV ELEMENT
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={"content-container"}>
+        <Navbar />
+        <h1>Hello World</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer className={"footer--pin"}>
+        Github {"=>"} <a href="https://github.com/Ishan-Choudhary/Todo_web_app" target="_blank">Leave a star if you liked it :)</a>
+      </footer>
     </>
   )
 }
 
-export default App
+export default App;
